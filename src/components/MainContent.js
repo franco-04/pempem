191bbcbb8a662e2d4cae7ba78969e32f9476ea94
AnetteeYunuen <anetteyunuenruizm@gmail.com>
2024-12-03 22:45:47 -0600

    nuevo

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,6 +3,8 @@ import './styles/MainContent.css';
 import imagen1 from '../assets/imagen1.png'; // Importa la imagen desde la ruta de tu archivo
 import imagen2 from '../assets/imagen2.png'; // Importa la imagen desde la ruta de tu archivo
 import imagen4 from '../assets/imagen4.png'; // Importa la imagen desde la ruta de tu archivo
+import imagen5 from '../assets/imagen5.png'; // Importa la imagen desde la ruta de tu archivo
+import imagen6 from '../assets/imagen6.png'; // Importa la imagen desde la ruta de tu archivo
 
 
 function MainContent() {
@@ -48,6 +50,7 @@ function MainContent() {
               <div className="hexagon"></div>
             </div>
             <h3>Pem Pem</h3>
+            
             <div className="hexagons3">
               {/* Puedes añadir hexágonos decorativos aquí */}
               <div className="hexagon"></div>
@@ -57,8 +60,8 @@ function MainContent() {
               <div className="hexagon"></div>
             </div>
             <h2>"La comunicación transforma, la mariposa te guía"</h2>
-            <button>Comienza ahora y permítenos acompañar a tu pequeño</button>
-          </div>
+            <button className="main-content-button">Comienza ahora y permítenos acompañar a tu pequeño</button>
+            </div>
         </div>
       </main>
 
@@ -82,16 +85,28 @@ function MainContent() {
               <p> En México existen más de 700 millones de personas, sufren un trastorno que no solo afecta sus habilidades comunicación sino la autoestima y la socialización.
               Debido a esto PEM PEM fue creado como una herramienta para padres y maestros con el deseo de apoyar a niños a partir de los 3 años hasta los 9 con problemas de lenguaje y lectoescritura a mejorar la forma en la que se comunican y poder transmitir sus necesidades y darle voz a lo que piensan, observan y sienten.</p>
           </div>
+          <section className="images">
+              <img src={imagen6} alt="Niña con madre" />
+          </section>
         </div>
 
 
         <div className="content-container2">
-          <h5>Nuestro enfoque y objetivo</h5>
-            <div className="text">
-              <p> En México existen más de 700 millones de personas, sufren un trastorno que no solo afecta sus habilidades comunicación sino la autoestima y la socialización.
-              Debido a esto PEM PEM fue creado como una herramienta para padres y maestros con el deseo de apoyar a niños a partir de los 3 años hasta los 9 con problemas de lenguaje y lectoescritura a mejorar la forma en la que se comunican y poder transmitir sus necesidades y darle voz a lo que piensan, observan y sienten.</p>
-          </div>
-        </div>
+  {/* Contenedor para título e imagen */}
+  <div className="title-image-container">
+    <h5>¿Qué es y cómo funciona?</h5>
+    <img src={imagen5} alt="Descripción de la imagen" className="imagen-debajo-del-titulo" />
+  </div>
+
+  {/* Contenedor para el texto */}
+  <div className="text-container">
+    <p>
+      PEM PEM es el sitio web que cuenta con estrategias que ayudará a tus pequeños a desarrollar habilidades lingüísticas mediante ejercicios que agilizan el aprendizaje. Nosotros sabemos que cada niño es diferente, por esto como primer paso te pedimos que contestes un formulario para conocer mejor tu caso y poderte brindar una ayuda más personalizada a tus necesidades. PEM PEM ofrece un amplio catálogo de ejercicios brindados por especialistas, y que se apegan a cada niño, con instrucciones claras para llevarlos a cabo de manera satisfactoria e incluso desde el hogar.
+    </p>
+  </div>
+</div>
+
+
 
 
         
